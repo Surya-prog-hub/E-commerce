685ef5a500a0953dba8c6b87a328c076440993d9
Suryakanta swain <124809444+Surya-prog-hub@users.noreply.github.com>
2023-10-05 13:37:39 +0530

    E-commerce website
    #ecommercewebsite #html #css #javascript

---
+++ style.js
@@ -0,0 +1,63 @@
+
+// ==========swiper category============/
+var swiperCategories = new Swiper(".categories__container", {
+  spaceBetween:24,
+  loop:true,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 2,
+          spaceBetween: 20,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 40,
+        },
+        1400: {
+          slidesPerView: 6,
+          spaceBetween: 24,
+        },
+      },
+    });
+    // swiper products
+    var swiperProducts= new Swiper(".new__container", {
+      spaceBetween:24,
+      loop:true,
+          navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+          },
+          breakpoints: {
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+              slidesPerView: 4,
+              spaceBetween: 40,
+            },
+            1400: {
+              slidesPerView: 4,
+              spaceBetween: 24,
+            },
+          },
+        });
+    // product tabs
+    const tabs=document.querySelectorAll('[data-target]'),
+    tabContents=document.querySelectorAll('[content');
+    tabs.forEach((tab) => {
+      tab.addEventListener('click',()=>{
+        const target=document.querySelector(tab.dataset.target);
+         tabContents.forEach((tabContent)=>{
+          tabContent.classList.remove('active-tab');
+         });
+         target.classList.add('active-tab');
+         tab.forEach((tab)=>{
+          tab.classList.remove('active-tab');
+         });
+         tab.classList.add('active-tab');
+      });
+    });
